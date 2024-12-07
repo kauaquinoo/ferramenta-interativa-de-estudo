be0314a1552daa7b5da772746bb8f0d975eb096c
kauaquinoo <kaua.nascimentoaquino@gmail.com>
2024-12-07 09:25:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que significa "variável" em programação e para que serve?',
+    '"Caixinha" que guarda dados para usar no programa.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente em área do mundo?',
+    'Ásia é o maior continente.'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é a diferença entre um "for" e um "while" em lógica de repetição?',
+    'For: repetição com número fixo, While: repetição com condição.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se traduz a frase "I am learning to code" para o português?',
+    '"I am learning to code" = "Eu estou aprendendo a programar".'
+)
